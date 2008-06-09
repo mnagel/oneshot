bf18dfbb49ec0ee5feb6703918da2e685a4315d8
Michael Nagel <michael.nagel@devzero.de>
2008-06-09 19:45:35 +0000

    hab irgendwo doch noch eine neuere version gefunden...
    git-svn-id: https://svn.devzero.de/noame/oneshot@44 ca13e8c8-dc7f-44b0-95cf-6e01cdd6d2ca

--- lib/oneshot.rb
+++ lib/oneshot.rb
@@ -1,20 +1,20 @@
 #!/usr/bin/env ruby 
 
-# TODO DONE generate dir-listing
-# TODO DONE generate ttl-file
-# TODO handle file path. locally allow, remote -> flatten (if local is path and remote nil, or remote path)
-# TODO cleanup code
-# TODO cleanup switches
+# DONE generate dir-listing
+# DONE generate ttl-file
+# DONE handle file path. locally allow, remote -> flatten (if local is path and remote nil, or remote path)
+# DONE cleanup code
+# DONE cleanup switches
 # TODO document code
 # TODO revise verbosity
 # TODO revise fakeness
 #
 # round 2 :
 # TODO handle wrong command line switches
-# TODO WONTFIX support scp --> does not allow mkdir...
+# WONTFIX support scp --> does not allow mkdir...
 # TODO remove temporary files
 # TODO include information about oneshot into html listing
-# TODO warn multiple files same name
+# WONTFIX warn multiple files same name
 # TODO handle spaces in filenames, remove(?) sftp does not like them
 # TODO above applies for descpription used in URL, too. its ok on html-list, though
 # TODO make nicer output (listing html)
@@ -23,16 +23,46 @@
 # TODO revise sftp return code handling
 # TODO revise sftp output redirection
 # TODO allow generation of config-file
+# TODO if local = folder -> warn, let shell glob!
+# TODO make remote files world-readable...
+# TODO more verbose help, naming files, usage, ... flattening of path
+# TODO check if any files...
+# TODO handle local files with spaces... cant be read because already escaped...
 # 
+# round 3 :
+# TODO fail if there is no input file...
+
 # delayed: 
 # TODO generate cleanup tool
-#
-
 
 require 'digest/sha1'
 require 'time'
 
-THEVERSION = "0.0.1"
+LOG_ERROR		= -1
+LOG_OUTPUT      =  0
+LOG_INFO		=  1
+LOG_DEBUG		=  2
+
+THEVERSION = "0.0.3"
+
+def log string, loglevel
+  puts string unless @options.verbosity < loglevel
+end
+
+def pad pre, char, post, len
+	pre = '' if pre.nil?
+	post = '' if post.nil?
+  return pre + char * [len - pre.length - post.length, 0].max + post
+end
+
+def quote string
+  return '"' + string + '"'
+end
+
+def tempfilename
+  hsh = Digest::SHA1.hexdigest(rand(2**32).to_s).slice(0..7)
+  return "/tmp/oneshot-#{hsh}.htm"
+end
 
 # file lib/shellwords.rb, line 69
 def shellescape(str)
@@ -53,194 +83,191 @@ def shellescape(str)
   return str
 end
 
-@options = Struct.new( 
-  :subject,
-  :ttl,
-  :configfile,
-  :verbosity,
-  :fakeness,
-  :host,
-  :user,
-  :prefix,
-  :port,
-  :httppre
-).new
+def asciify string
+	return string.tr("^A-Za-z0-9_.\-", "_")
+end
+
+@options = Struct.new(:title, :ttl, :configfile, :verbosity, :fakeness,
+  :host, :user, :port, :prefix, :httppre).new
 
 def options_per_default
-  @options.subject = nil
-  @options.ttl = 30
-  @options.configfile = ENV['HOME'] + '/.nscripts/oneshot-cfg.rb'
-  @options.verbosity = 0
-  @options.fakeness = 0
-  @options.host = nil
-  @options.user = nil
-  @options.prefix = nil
-  @options.port = nil
-  @options.httppre = nil
+  @options.title			= nil
+  @options.ttl				= 30
+  @options.configfile		= ENV['HOME'] + '/.oneshot-cfg.rb'
+  @options.verbosity		= 0
+  @options.fakeness			= 0
+  @options.host				= nil
+  @options.user				= nil
+  @options.prefix			= nil
+  @options.port             = nil
+  @options.httppre			= nil
 end
 
-def log string, level
-  puts string unless @options.verbosity < level
+def create_new_config
+  file = <<EOT
+# put the following in your ~/.oneshot-cfg.rb (change as needed, of course)
+# the file is not created automatically, but you can use shell redirection if you want :)
+# created by oneshot #{THEVERSION} on #{Time.now.strftime("%Y-%m-%d %H:%M:%S")}
+#
+@options.title = 'untitled' if @options.title.nil?
+# @options.ttl = 30
+# @options.configfile = ENV['HOME'] + '/.oneshot-cfg.rb'
+# @options.verbosity = 0
+# @options.fakeness = 0
+@options.host = "myhost.example"  if @options.host.nil?
+# @options.user = nil
+@options.prefix = "var/www/oneshot/"  if @options.prefix.nil?
+# @options.port = nil if @options.port.nil?
+@options.httppre = 'http://myhost.example/oneshot/' if @options.httppre.nil?
+EOT
 end
 
-class TRANSFER < Struct.new(:local, :remote, :desc, :urlhttp); end
-class SWITCH < Struct.new(:char, :comm, :args, :code); end
+Transfer = Struct.new(:path_local, :path_remote, :description, :url_http)
+Switch = Struct.new(:char, :comm, :args, :code)
 
-def options_from_file file
+def options_from_file filename
   begin
-	log "gonna load config file", 2
-	log "config file not found", 0 unless File.exist?(file)
-	if File.exist?(file)
-	  load file
-	  log "loaded config file", 2
-    end
+		log "gonna load config file", LOG_DEBUG
+		log "config file not found", LOG_ERROR unless File.exist?(filename)
+		if File.exist?(filename)
+			load filename
+			log "loaded config file", LOG_INFO
+		end
   rescue => exc # FIXME does not seem to work when removing the file.exists?
-	log "error loading config file", 0
+		log "error loading config file", LOG_ERROR
   end
 end
 
 def options_from_cmd
-  log ARGV.join(' '), 2
+  log ARGV.join(' '), LOG_DEBUG
 
-  currentTransfer = TRANSFER.new()
+  currentTransfer = Transfer.new()
   @transfers = []
-  switches = nil  
-  switches = [
+	switches = nil # for scoping
+	@helpswitch = Switch.new('h', 'print help message',	false, proc { switches.each { |e| puts '-' + e.char + "\t" + e.comm }; Process.exit })
+	switches = [
+		Switch.new('f', 'specify remote filename for next file',	true, proc { |val| currentTransfer.path_remote = val }),
+		Switch.new('d', 'specify remote description for next file', true, proc { |val| currentTransfer.description = val }),
+		Switch.new('t', 'specify title used in URL',				true, proc { |val| @options.title = val }),
 
-	SWITCH.new('f', 'specify remote filename for next file',	true, proc { |val| currentTransfer.remote = val }),
-	SWITCH.new('d', 'specify remote description for next file', true, proc { |val| currentTransfer.desc = val }),
-	SWITCH.new('s', 'specify subject used in URL',				true, proc { |val| @options.subject = val }),
+		Switch.new('x', 'specify ttl, remote file can be deleted then', true, proc { |val| @options.ttl = val }),
 
-	SWITCH.new('e', 'specify ttl, remote file can be deleted then', true, proc { |val| @options.ttl = val }),
+		Switch.new('c', 'specify local configfile', true, proc { |val| @options.configfile = val }),
 
-	SWITCH.new('c', 'specify local configfile', true, proc { |val| @options.configfile = val }),
+		Switch.new('o', 'specify sftp host',        	true, proc { |val| @options.host = val }),
+		Switch.new('u', 'specify sftp user',			true, proc { |val| @options.user = val }),
+		Switch.new('l', 'specify remote base location',	true, proc { |val| @options.prefix = val }),
+		Switch.new('p', 'specify sftp port',			true, proc { |val| @options.port = val }),
 
-	SWITCH.new('o', 'specify sftp host',			true, proc { |val| @options.host = val }),
-	SWITCH.new('u', 'specify sftp user',			true, proc { |val| @options.user = val }),
-	SWITCH.new('l', 'specify remote base location', true, proc { |val| @options.prefix = val }),
-	SWITCH.new('p', 'specify sftp port',			true, proc { |val| @options.port = val }),
-	
-	SWITCH.new('x', 'specify http prefix',	true, proc { |val| @options.httppre = val }),
+		Switch.new('e', 'specify http prefix',	true, proc { |val| @options.httppre = val }),
 
-	SWITCH.new('v', 'increase verbosity',	false, proc { @options.verbosity += 1 }),
-	SWITCH.new('w', 'increase fakeness',	false, proc { @options.fakeness += 1 }),
+		Switch.new('v', 'increase verbosity',	false, proc { @options.verbosity += 1 }),
+		Switch.new('w', 'increase fakeness',	false, proc { @options.fakeness += 1 }),
+		
+		Switch.new('i', 'create new config file',   false, proc { log(create_new_config, 0) ; Process.exit }),
+		@helpswitch
+	]
 
-	SWITCH.new('h', 'print help message',	false, proc { switches.each { |e| puts '-' + e.char + "\t" + e.comm }; Process.exit })
-  ]
+  onfile = proc { |filename| currentTransfer.path_local = filename; @transfers << currentTransfer; currentTransfer = Transfer.new()}
 
-  onfile = proc { |filename| currentTransfer.local = filename; @transfers << currentTransfer; currentTransfer = TRANSFER.new()}
+  notargs = [] 
 
-  eatarg = -1 
+  ARGV.each_index { |i|
+		next if notargs.include?(i)
 
-  ARGV.each_index do |i|
-	next if i == eatarg
+		arg = ''.replace(ARGV[i]) #FIXME: unfreeze
 
-	arg = ''.replace(ARGV[i]) # unfreeze!
-	# puts "parsing #{arg}"
-
-	if arg.slice(0..0) == '-'
-	  arg.slice(1..-1).scan(/./) do |chr|
-		myswitch = switches.select{|s| s.char == chr}.first
-		myswitch = switches.last if myswitch.nil?
-		if myswitch.args == true
-		  myswitch.code.call(ARGV[i+1])
-		  eatarg = i+1
+		if arg[0..0] == '-'
+			arg[1..-1].scan(/./) do |chr|
+				myswitch = switches.find {|s| s.char == chr}
+				myswitch = @helpswitch if myswitch.nil?
+				if myswitch.args
+					myswitch.code.call(ARGV[i+1])
+					notargs << i+1
+				else
+					myswitch.code.call
+				end
+			end
 		else
-		  myswitch.code.call
+			onfile.call(ARGV[i])	  
 		end
-	  end
-	else
-	  onfile.call(ARGV[i])	  
-	end
-  end
-end
-
-def pad pre, char, len
-  return pre + char * [len - pre.length, 0].max
-end
-
-def rpad pre, char, len
-  return char * [len - pre.length, 0].max + pre
-end
-
-def q s
-  return '"' + s + '"'
+  }
 end
 
 def sanatize_options
-  @options.subject = shellescape @options.subject
+  @options.title = asciify(@options.title)
   @options.ttl = @options.ttl.to_s
   #@options.configfile = ENV['HOME'] + '/.nscripts/oneshot-cfg.rb'
   #@options.verbosity = 0
   #@options.fakeness = 0
   #@options.host = nil
   #@options.user = nil
-  #@options.prefix = nil
+  @options.prefix = '' if @options.prefix.nil?
   @options.port = @options.port.to_s unless @options.port.nil?
   #@options.httppre = nil
-	 
+  
   @transfers.each { |t|
-	t.local = shellescape t.local
-	t.remote = shellescape t.remote unless t.remote.nil?
+    t.path_local  = shellescape t.path_local
+    t.path_remote = t.path_local if t.path_remote.nil?
+    t.path_remote = File.basename(t.path_remote)
+    t.path_remote = asciify shellescape(t.path_remote)	
   }
 end
 
 def print_options
-  log "options:\n", 1
+  log "options:\n", LOG_INFO
   @options.each_pair { |name, val|
-	name = name.to_s
-	val = val.to_s
-	log pad(name, ' ', 10) + " = " + val.to_s, 1
+		name = name.to_s
+		val = val.to_s
+		log pad(name, ' ', nil, 10) + " = " + val.to_s, LOG_INFO
   }
   
   @transfers.each { |t| 
-	a = 'upload: ' + q(t.local)
-	a += ' goes to ' + q(t.remote) unless t.remote.nil? 
-	a += ' tagged ' + q(t.desc) unless t.desc.nil? 
-	log a, 1
+		tmp  = 'upload: '		+ quote(t.path_local)
+		tmp += ' goes to '	+ quote(t.path_remote) unless t.path_remote.nil? 
+		tmp += ' tagged '		+ quote(t.description) unless t.description.nil? 
+		log tmp, LOG_INFO
   }
 end
 
-def create_sftp_commands
-  date = Time.now.strftime("%Y-%m-%d") # -%H:%M-%S")
-  hsh = Digest::SHA1.hexdigest(rand(2**32).to_s).slice(0..15)
-
-  sftp_commands  = ""
-  sftp_commands += "EOF\n"
-  sftp_commands += "-mkdir #{date}\n"
-  sftp_commands += "cd #{date}\n"
-  sftp_commands += "-mkdir #{@options.subject}\n"
-  sftp_commands += "cd #{@options.subject}\n"
-  sftp_commands += "mkdir #{hsh}\n"
-  sftp_commands += "cd #{hsh}\n"
-
+def create_sftp_commands tfn, ttlname
+  date = Time.now.strftime("%Y-%m-%d")
+  hsh  = Digest::SHA1.hexdigest(rand(2**32).to_s).slice(0..15)
+	
+  cmds  = ""
+  cmds += "EOF\n"
+  cmds += "-mkdir #{date}\n"
+  cmds += "cd #{date}\n"
+  cmds += "-mkdir #{@options.title}\n"
+  cmds += "cd #{@options.title}\n"
+  cmds += "mkdir #{hsh}\n"
+  cmds += "cd #{hsh}\n"
+	
+	prefix = @options.httppre + date + '/' + @options.title + '/' + hsh + '/'
+	
   @transfers.each { |t|
-	sftp_commands += "put #{t.local} #{t.remote}\n"
-	t.urlhttp = @options.httppre + date + '/' + @options.subject + '/' + hsh + '/' + (t.remote.nil? ? t.local : t.remote)
+		cmds += "put #{t.path_local} #{t.path_remote}\n"
+		cmds += "chmod 644 #{t.path_remote}\n"
+		t.url_http = prefix + t.path_remote
   }
   
-  sftp_commands += "put #{@tfn} index.htm\n"
-  sftp_commands += "put #{@ttlname} .oneshot-expiry\n"
-  sftp_commands += "EOF\n"
+  cmds += "put #{tfn} index.htm\n"
+  cmds += "put #{ttlname} .oneshot-expiry\n"
+  cmds += "EOF\n"
   
-  @idxrem = @options.httppre + date + '/' + @options.subject + '/' + hsh + '/index.htm'
+  @idxrem = prefix + 'index.htm'
   
-  return sftp_commands
+  return cmds
 end
 
-def tempfilename
-  hsh = Digest::SHA1.hexdigest(rand(2**32).to_s).slice(0..7)
-  return "/tmp/oneshot-#{hsh}.htm"
-end
-
-def generate_dir_list expiry
-  result = <<EOT
+def generate_dir_list_helper subject, table
+<<EOT
 <?xml version="1.0" ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 
 <html xmlns="http://www.w3.org/1999/xhtml">
   <head>
-    <title>oneshot - upload - #{@options.subject}</title>
+    <title>oneshot - upload - #{subject}</title>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <style type="text/css">
       * { 
@@ -253,38 +280,58 @@ def generate_dir_list expiry
   </head>
   <body>
 
-    <h1>oneshot - upload - #{@options.subject}</h1>
+    <h1>oneshot - upload - #{subject}</h1>
     <div>
-
-	#{  res = ''
-  @transfers.each { |t|
-  temp = ''
-  name = t.remote.nil? ? t.local : t.remote
-  temp += '<h2><a href="' + name + '">' + name + '</a></h2>'
-  temp +='<ul>'
-  desc = t.desc #t.desc.nil? ? "no description" : t.desc
-  temp += '<li>' + desc + '</li>' unless t.desc.nil?
-  date = Time.now.strftime("%Y-%m-%d %H:%M:%S")
-  date2 = expiry.strftime("%Y-%m-%d %H:%M:%S")
-  temp += '<li>uploaded: ' + date + '</li>'
-  temp += '<li>min. online until: ' + date2 + '</li>'
-  hash = "sha1: " + Digest::SHA1.hexdigest(File.read(t.local))
-  temp += '<li>' + hash + '</li>'
-  temp += "</ul>\n"
-  res += temp
-  }
-  res
-  }  
-  </div>
+		#{table}
+		</div>
 
   </body>
-  </html>
+</html>
 EOT
-  
+end
 
-  
-  log result, 2
+def generate_dir_list expiry
+	table = @transfers.map { |t|
+		temp = ''
+		temp += '<h2><a href="' + t.path_remote + '">' + t.path_remote + '</a></h2>'
+	
+		temp +='<ul>'
+		
+		temp += '<li>' + t.description + '</li>' unless t.description.nil?
+		
+		date = Time.now.strftime("%Y-%m-%d %H:%M:%S")
+		date2 = expiry.strftime("%Y-%m-%d %H:%M:%S")
+		temp += '<li>uploaded: ' + date + '</li>'
+		temp += '<li>min. online until: ' + date2 + '</li>'
+
+		hash = "sha1: " + Digest::SHA1.hexdigest(File.read(t.path_local))
+		temp += '<li>' + hash + '</li>'
+		
+		temp += "</ul>\n"
+  }
+	
+	result = generate_dir_list_helper @options.title, table
+	
+  log result, LOG_DEBUG
   return result
+end
+
+def run_sftp commands
+	real_command = "sftp -C #{@options.host}#{':' + @options.prefix} 1> /dev/null 2>/dev/null << #{commands}"
+  log real_command, LOG_INFO
+  log '### oneshot: calling sftp ###', LOG_DEBUG
+  system real_command if @options.fakeness == 0
+  log '### oneshot: called  sftp ###', LOG_DEBUG
+  state = $?
+  log "command returned: " + state.to_s, LOG_INFO
+  log "SFTP ERROR!!!", LOG_ERROR unless state == 0 
+  log '----- ----- ----- -----', LOG_INFO
+	  
+	@transfers.each { |t|
+		log t.url_http, LOG_INFO
+  } if state == 0
+	
+	return state
 end
 
 begin
@@ -294,49 +341,30 @@ begin
   
   sanatize_options
   print_options
+	
+	if @transfers.empty?
+		log "found no files to transfer\n\n", LOG_ERROR
+		@helpswitch.code.call	
+  end
   
-  @tfn = tempfilename
-  @ttlname = tempfilename + '.ttl'
+	tempfile_list = tempfilename
+  tempfile_expire = tempfilename + '.ttl'
   
-  expiry = (Time.now + @options.ttl.to_f * 60 * 60 * 24)   #strftime("%Y-%m-%d") # -%H:%M-%S")
-  # puts expiry
-  #s = Time.parse(expiry)
-  log expiry.strftime("%Y-%m-%d-%H:%M:%S"), 1
+  expiry = Time.now + @options.ttl.to_f * 60 * 60 * 24
+  log "expires on " + expiry.strftime("%Y-%m-%d-%H:%M:%S"), LOG_INFO
   
-  sftp_commands = create_sftp_commands
-  mystring = generate_dir_list expiry
+  sftp_commands = create_sftp_commands tempfile_list, tempfile_expire
+  htmllist = generate_dir_list expiry
   
-
-  
-  filename = @tfn
-  File.open filename, 'w+' do |f|
-	f.puts mystring
-  end 
-  
-  filename = @ttlname
-  File.open filename, 'w+' do |f|
-	f.puts expiry
-  end 
-  
-  real_command = "sftp -C #{@options.host}#{':' + @options.prefix} 1> /dev/null 2>/dev/null << #{sftp_commands}"
-  log real_command, 1
-  log '### oneshot: calling sftp ###', 2
-  system real_command if @options.fakeness == 0
-  log '### oneshot: called  sftp ###', 2
-  state = $?
-  log "command returned: " + state.to_s, 1
-  log "SFTP ERROR!!!", 0 unless state == 0 
-  log '----- ----- ----- -----', 1
-  
-  @transfers.each { |t|
-	log t.urlhttp, 1
-  } if state == 0
-  log @idxrem, 0 if state == 0
-
+  File.open(tempfile_list, 'w+') { |f| f.puts htmllist } 
+  File.open(tempfile_expire, 'w+') { |f| f.puts expiry } 
+	
+	state = -1
+	state = run_sftp sftp_commands
+  log @idxrem, LOG_OUTPUT if state == 0
+	
 rescue => exc
-  STDERR.puts "there was an error"
-  STDERR.puts exc.backtrace * "\n"
-  STDERR.puts "E: #{exc.message}"
-  #STDERR.puts ARGV.to_s
+  STDERR.puts "there was an error: #{exc.message}"
+  STDERR.puts exc.backtrace
   exit 1
 end
